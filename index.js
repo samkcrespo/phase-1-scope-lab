db7c10c554b4aaebc4219e49d9f2f8934928e7aa
Samantha Crespo <samkcrespo@gmail.com>
2022-04-05 22:25:48 -0700

    commit tree 015111165cf74abb1124f5371e1f6a2e08f2aa7f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'bill'
+
+function upperCaseCustomerName () {
+  customerName = customerName.toUpperCase(); 
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer () {
+leastFavoriteCustomer = 'karen'
+}
+
+// function upperCaseCustomerName () {
+   // var customerName = customerName.toUpperCase();
+//};
 // Write your solution in this file!
